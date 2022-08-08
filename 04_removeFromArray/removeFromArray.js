6d8774529e406a6f7cec316628d70ea7f5f5decb
Tahzeeb <ishmum20000@gmail.com>
2022-08-09 00:24:42 +0600

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    newArr = arr;
+    args.forEach(element => {
+        for (let i = newArr.length - 1; i >= 0; i --) {
+            if (element === newArr[i]) {
+                newArr.splice(i, 1);
+            }
+        }
+    });
 
+    return newArr;
 };
 
 // Do not edit below this line
