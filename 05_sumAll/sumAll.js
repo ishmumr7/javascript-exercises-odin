88f0ec647cf1bdb59e3a03d5201b07b9d02c7bcb
Tahzeeb <ishmum20000@gmail.com>
2022-08-09 00:59:01 +0600

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(startNum, finalNum) {
+    if (startNum < 0 || finalNum < 0) {
+        return "ERROR";
+    }
 
+    if (typeof startNum != 'number' || typeof finalNum != 'number') {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    if (startNum < finalNum) {
+        for (let i = startNum; i <= finalNum; i ++) {
+            sum += i;
+        }
+    } else if (startNum >= finalNum) {
+        for (let i = startNum; i >= finalNum; i --) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
