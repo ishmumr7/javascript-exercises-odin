0506aa02f5ea0532102ca5fb2ff0743ea3293d61
Tahzeeb <ishmum20000@gmail.com>
2022-08-23 23:03:32 +0600

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    var punctuationless = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 
+    var finalStr = punctuationless.replace(/\s+/g, "");
+
+    var str_rev = [...finalStr].reverse().join("");
+
+    return (finalStr.toLowerCase() === str_rev.toLowerCase());
 };
 
 // Do not edit below this line
