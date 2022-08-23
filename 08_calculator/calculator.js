a8f8e0b99f9ddff8776cfe795099b6db39f6eb3e
Tahzeeb <ishmum20000@gmail.com>
2022-08-23 22:21:30 +0600

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    total += nums[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let result = 1;
+  nums.forEach(num => {
+    result *= num;
+  });
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
-};
+// const factorial = function(num) {
+//   if (num === 0) {
+//     return 1;
+//   }
+
+//   let result = 1;
+//   for (let i = num; i > 0; i--) {
+//      result *= i;
+//   }
+
+//   return result;
+// };
+
+//Another implementation of factorial
+
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  
+  return num * factorial(num - 1);
+}
 
 // Do not edit below this line
 module.exports = {
