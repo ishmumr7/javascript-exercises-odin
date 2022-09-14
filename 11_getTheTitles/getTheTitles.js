ac92fd19163dc7f9fcbf4fe92cb29a87a4c56423
Tahzeeb <ishmum20000@gmail.com>
2022-09-14 19:49:42 +0600

    Solve 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (things) {
+    const names = things.map(thing => thing.title);
+    return names;
 };
 
 // Do not edit below this line
